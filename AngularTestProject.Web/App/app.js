2b2f6eb840d8737ebed88765c726ef93d98228a5
Alexander Teseyko <alexteko@gmail.com>
2015-03-27 23:20:19 +0300

    1

---
+++ AngularTestProject.Web/App/app.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    var gems = [{ name: 'Azurite', price: 2.95, canPurchase: true, soldOut: false }, { name: 'Fdsfsdf', price: 2.00, canPurchase: true, soldOut: false }, ];
+    var app = angular.module('gemStore', []);
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+})();
